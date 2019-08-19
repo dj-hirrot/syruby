45d54461ca6e61c8f3fee6cc82e45aad49a0e0bc
HirotoShibutani <creation.199484@gmail.com>
2019-08-19 19:06:31 +0900

    [update] APIリクエストにかかるものの追加

--- app/controllers/api/v1/main/home_controller.rb
+++ app/controllers/api/v1/main/home_controller.rb
@@ -1,12 +1,9 @@
 class Api::V1::Main::HomeController < Api::V1::ApplicationController
   def get_test_text
-    p "000000000000000000000000000000000000000"
-    p params.inspect
-    p home_params.inspect
-    p home_params[:inputText]
-    p "000000000000000000000000000000000000000"
+    response = Api::GooLab.to_ruby(home_params[:inputText])
+
     result = {
-      testText: home_params[:inputText]
+      testText: response
     }
 
     render json: result
