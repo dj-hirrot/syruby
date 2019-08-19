7cc2fe50f13629d30b36dd6e37763b7f010d37c3
HirotoShibutani <creation.199484@gmail.com>
2019-08-19 17:39:52 +0900

    [update] json形式でレスポンスする

--- app/controllers/api/v1/main/home_controller.rb
+++ app/controllers/api/v1/main/home_controller.rb
@@ -1,9 +1,23 @@
-class Api::V1::Main::HomeController < ApplicationController
-  def get_test_text(inputText)
+class Api::V1::Main::HomeController < Api::V1::ApplicationController
+  def get_test_text
+    p "000000000000000000000000000000000000000"
+    p params.inspect
+    p home_params.inspect
+    p home_params[:inputText]
+    p "000000000000000000000000000000000000000"
     result = {
-      testText: inputText
+      testText: home_params[:inputText]
     }
 
-    return result
+    render json: result
   end
+
+  private
+    def home_params
+      params.require(:home).permit(home_param_items)
+    end
+
+    def home_param_items
+      %i[inputText]
+    end
 end
